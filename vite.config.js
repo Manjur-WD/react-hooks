96b00751e46116bc3a17468f96d4a522b9b16314
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2024-11-20 19:37:45 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         display: "standalone", // App behaves like a native app
         icons: [
           {
-            src: "/icons/icon-192x192.png",
+            src: "https://manjur-wd.github.io/icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "https://manjur-wd.github.io/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
